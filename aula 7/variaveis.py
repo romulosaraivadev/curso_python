3fde3874e8a554d977314ed962545889e1fe8a04
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-08-05 07:55:42 -0300

    operadores e variaveis

--- aula 7/variaveis.py
+++ aula 7/variaveis.py
@@ -2,5 +2,13 @@
 # PEP8: inicie variáveis com letras minúsculas, pode usar numeros e undeline __.
 # O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variavel).
 # Uso: nome_variavel = expressão
-nome_completo = 'Romulo de Oliveira Saraiva'
-print(nome_completo)
+
+# nome_completo = 'Romulo de Oliveira Saraiva'
+# soma_dois_mais_dois = 2+2
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Romulo'
+idade = 18
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
---
+++ aula 9/operadore.py
@@ -0,0 +1,20 @@
+adicao = 10+10
+print('Adição', adicao)
+
+subtracao = 10-5
+print('subtração', subtracao)
+
+multiplicacao = 10*10
+print('multiplicação', multiplicacao)
+
+divisao = 10/2.2
+print('divisão', divisao)
+
+divisao_inteiras = 10//2.2
+print('divisão inteira', divisao_inteiras)
+
+exponenciacao = 2 ** 10
+print('exponenciação', exponenciacao)
+
+modulo = 55%2 #resto da divisao
+print('módulo', modulo)
