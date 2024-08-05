9f2d47e05222f98424013e7abee0964df49681f3
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-08-05 07:32:28 -0300

    novas aulas

--- aula 6/coercao_tipos.py
+++ aula 6/coercao_tipos.py
@@ -3,5 +3,7 @@
 # é o ato de converter um tipo em outro
 # tipos imutáveis e primitivos:
 # str, int, float, bool
-print(1+1)
-print('a' + 'b')
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11)+'b')
---
+++ aula 7/variaveis.py
@@ -0,0 +1,6 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar numeros e undeline __.
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variavel).
+# Uso: nome_variavel = expressão
+nome_completo = 'Romulo de Oliveira Saraiva'
+print(nome_completo)
