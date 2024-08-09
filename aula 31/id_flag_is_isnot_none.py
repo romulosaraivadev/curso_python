3fd96d9192971acc142877692a84041fed0b0838
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-08-09 14:33:09 -0300

    nn

---
+++ aula 31/id_flag_is_isnot_none.py
@@ -0,0 +1,20 @@
+'''
+Flag (Vandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, idadentidade)
+id = identidade
+'''
+
+# v1 = 'a'
+# v2 = 'b'
+# print(id(v1))
+# print(id(v2))
+
+
+
+condicao = False
+
+if condicao:
+    print('Façã algo')
+else:
+    print('Não faça algo')
