2760a686ef08a3b456cd669b22b7839c9b114074
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-08-12 10:47:00 -0300

    while

--- aula 34/while_beak.py
+++ aula 34/while_beak.py
@@ -4,12 +4,32 @@ while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 loop infinito -> quando um codigo não tem fim
 '''
-condicao = True
+# condicao = True
 
-while condicao:
-    nome = input('Qual o seu nome: ')
-    print(f'Seu nome é {nome}')
+# while condicao:
+#     nome = input('Qual o seu nome: ')
+#     print(f'Seu nome é {nome}')
 
-    if nome == 'sair':
+#     if nome == 'sair':
+#         break
+# print('Acabou')
+
+contador = 0
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue
+
+    if contador >= 10 and contador <= 27:
+
+        continue
+
+    print(contador)
+
+    if contador == 40:
         break
+    
 print('Acabou')
---
+++ aula 35/operadores_atribuicao.py
@@ -0,0 +1,9 @@
+'''
+Operadores de atribuição
+= += -= /= //= **= %=
+'''
+contador = 1
+
+
+contador += 2
+print(contador)
