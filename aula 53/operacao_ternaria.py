c5f23ab275f12ded68efdd6f778e484efaad4df2
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-09-04 13:34:36 -0300

    km

---
+++ aula 53/operacao_ternaria.py
@@ -0,0 +1,13 @@
+'''
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor
+'''
+# condicao = 10 == 11
+# variavel = 'Valor' if condicao else 'Outro valor'
+
+# print(variavel)
+
+# digito = 10 
+# novo_digito = digito if digito <= 9 else 0
+# print(novo_digito)
+
