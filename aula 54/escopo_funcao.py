00b82c14eaa1e8637e6aadb76420c7ac6ab34371
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-09-10 14:29:11 -0300

    Update escopo_funcao.py

--- aula 54/escopo_funcao.py
+++ aula 54/escopo_funcao.py
@@ -5,6 +5,9 @@ Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
+Não temos acesso a nomes de escopos internos nos escopos externos.
+A palavra flocal faz uma variável do escopo externo
+ser a mesma no escopo interno
 """
 x = 1
 
@@ -13,11 +16,13 @@ def escopo():
     x = 10
     
     def outra_funcao():
+        x=11
         y = 2
         print(x, y)
 
     outra_funcao()
     print(x)
 
-
+print(x)
 escopo()
+print(x)
